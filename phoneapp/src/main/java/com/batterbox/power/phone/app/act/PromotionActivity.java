25efe1f49bf42e6a3c9a2e54d639fdbd05151904
hezongfa <651780771@qq.com>
2020-04-14 00:17:51 +0800

    Merge branch 'master' of https://gitee.com/hezongfa/Battery2BoxApp
    # Conflicts:
    #	phoneapp/build.gradle
    #	phoneapp/libs/jcore-android-2.1.2-google_play.jar
    #	phoneapp/libs/jpush-android-3.3.4.jar

--- phoneapp/src/main/java/com/batterbox/power/phone/app/act/PromotionActivity.java
+++ phoneapp/src/main/java/com/batterbox/power/phone/app/act/PromotionActivity.java
@@ -12,17 +12,24 @@ import com.alibaba.android.vlayout.layout.LinearLayoutHelper;
 import com.batterbox.power.phone.app.BatterBoxApp;
 import com.batterbox.power.phone.app.R;
 import com.batterbox.power.phone.app.aroute.ARouteHelper;
+import com.batterbox.power.phone.app.dialog.ShopCouponDialog;
 import com.batterbox.power.phone.app.entity.LbsShopCouponEntity;
+import com.batterbox.power.phone.app.entity.LocMap;
 import com.batterbox.power.phone.app.http.HttpClient;
 import com.batterbox.power.phone.app.http.NormalHttpCallBack;
+import com.batterbox.power.phone.app.utils.LocMapUtil;
 import com.chenyi.baselib.entity.ResponseEntity;
 import com.chenyi.baselib.ui.NavListActivity;
 import com.chenyi.baselib.utils.ImageLoaderUtil;
+import com.chenyi.baselib.utils.MathsUtil;
 import com.chenyi.baselib.utils.StringUtil;
+import com.chenyi.baselib.utils.print.FQT;
+import com.chenyi.baselib.widget.dialog.DialogUtils;
 import com.chenyi.baselib.widget.vlayoutadapter.BaseViewHolder;
 import com.chenyi.baselib.widget.vlayoutadapter.QuickDelegateAdapter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import qiu.niorgai.StatusBarCompat;
 
@@ -76,6 +83,26 @@ public class PromotionActivity extends NavListActivity<LbsShopCouponEntity> {
                 holder.setText(R.id.item_promotion_location_tv, StringUtil.fixNullStr(item.shopAdress));
                 holder.setText(R.id.item_promotion_coupon_tv, StringUtil.fixNullStr(item.couponRemarke));
                 holder.setText(R.id.item_promotion_dis_tv, StringUtil.fixNullStr(item.distance));
+                holder.setOnClickListener(R.id.item_promotion_dis_tv, v -> {
+                    List<LocMap> locMaps = LocMapUtil.getLocMapList(PromotionActivity.this);
+                    if (locMaps.size() > 0) {
+                        ArrayList<String> list = new ArrayList<>();
+                        for (LocMap locMap : locMaps) {
+                            list.add(locMap.appName);
+                        }
+                        DialogUtils.showListDialog(PromotionActivity.this, null, list, position -> {
+                            LocMap locMap = locMaps.get(position);
+                            LocMapUtil.navigationByLocMap(PromotionActivity.this, MathsUtil.round(item.la, 6), MathsUtil.round(item.lo, 6), StringUtil.fixNullStr(item.shopName, "location"), StringUtil.fixNullStr(item.shopAdress), locMap.packageName);
+                        });
+                    } else {
+                        FQT.showShort(PromotionActivity.this, "no map app client");
+                    }
+                });
+                holder.itemView.setOnClickListener(v -> {
+                    ShopCouponDialog shopCouponDialog = new ShopCouponDialog();
+                    shopCouponDialog.setShopEntity(item);
+                    shopCouponDialog.show(getSupportFragmentManager());
+                });
             }
 
             @Override
